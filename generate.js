3b35befcc0148990ef25a6b3ff3c4fb5c328084c
plocket <52798256+plocket@users.noreply.github.com>
2021-02-07 08:21:27 -0500

    Add custom ignore and make pretty

--- generate.js
+++ generate.js
@@ -4,47 +4,17 @@
 // Cut down on unneeded data
 // ============================
 let keys_to_ignore = [
-  '_internal',
-  'nav',
-  'url_args',
-  '_class',
-  'auto_gather',
-  'instanceName',
-  'mimetype',  // Not sure about removing this one
-  'multi_user',
   // Less sure these are safe to ignore
   'ask_number',
   'ask_object_type',
   'object_type',
-  // 'there_are_any',  // This is a real field
   'gathered',
-  // Files
-  'file_info',
-  'filename',
-  'persistent',
-  'private',
-  'convert_to_pdf_a',
-  'convert_to_tagged_pdf',
-  'extension',
-  'valid_formats',
-  'encrypted',
-  'has_specific_filename',
-  // address
-  'geolocate_response',
-  'norm_long',
-  'city_only',
-  'geolocated',
-  'orig_address',
-  'latitude',
-  'longitude',
-  'norm',
-  'geolocate_success',
-  'complete_attribute',
 ];
 
+// Conceptual version
 // Ignore text we should ignore wherever it appears, even in a fully formed variable name
-let ignore_anywhere = [
-  // AssemblyLine-specific keywords to skip
+let ignore_anywhere_default = [
+  // === AssemblyLine-specific keywords to skip
   'interview_metadata',
   'macourts',
   'court_emails',
@@ -55,12 +25,42 @@ let ignore_anywhere = [
   '.uses_parts',
   `.court_code`,
   `.department`,
-  // `.description`,
+  // `.name`, `.phone`, `.description`,
   `.division`,
   `.fax`,
   `.has_po_box`,
-  // `.name`,
-  // `.phone`,
+
+  // === da
+  '_internal',
+  'nav',
+  'url_args',
+  '_class',
+  'auto_gather',
+  'instanceName',
+  'mimetype',  // Not sure about removing this one
+  'multi_user',
+  // --- files
+  'file_info',
+  'filename',
+  'persistent',
+  'private',
+  'convert_to_pdf_a',
+  'convert_to_tagged_pdf',
+  'extension',
+  'valid_formats',
+  'encrypted',
+  'has_specific_filename',
+  // --- address
+  'geolocate_response',
+  'norm_long',
+  'city_only',
+  'geolocated',
+  'orig_address',
+  'latitude',
+  'longitude',
+  'norm',
+  'geolocate_success',
+  'complete_attribute',
 ];
 
 
@@ -98,13 +98,14 @@ let add_story_row = function(name, choice, value, story) {
   if (ignore_anywhere.includes( name )) { return; }
   for ( let to_ignore of ignore_anywhere ) {
     if ( name.includes( to_ignore ) ) { return; }
+    // ignore choice too?
   }
 
-  if (typeof value === 'string') {
+  if (typeof value === 'string' ) {
     value = JSON.stringify(value);
     value = value.substring(1, value.length - 1);
     // DONE: Dates are dumb.
-    value = value.replace(/(\d\d\d\d)-(\d\d)-(\d\d)T\d\d:\d\d:\d\d-\d\d:\d\d/, '$2/$3/$1');
+    value = value.replace(/(\d\d\d\d)-(\d\d)-(\d\d)T\d\d:\d\d:\d\d-\d\d:\d\d/, '$2/$3/$1' );
   }
   let row = `| ${name} | ${choice} | ${value} |`;
   // If it isn't already in the variable table, add it
@@ -121,7 +122,7 @@ let parse_arr = function(name, arr, story) {
     let var_name = `${name}[${index}]`;
 
     let value = arr[index];
-    if (typeof value === 'string' || typeof value === 'number') {
+    if (typeof value === 'string' || typeof value === 'number' ) {
       add_story_row( var_name, '', value, story );
     } else if (Array.isArray(value)) {
       parse_arr( var_name, value, story );
@@ -133,7 +134,7 @@ let parse_arr = function(name, arr, story) {
         story: story
       });
     } else {
-      console.log('Sorry, something has not been accounted for. You will have to do some editing to get this to work. (2)', var_name, typeof value);
+      console.warn( 'Sorry, something has not been accounted for. You will have to do some editing to get this to work. (2)', var_name, typeof value);
     }
   }
 }
@@ -154,7 +155,7 @@ let parse_elements = function(var_name, elements, story) {
     for ( let element in elements ) {
       let element_value = elements[ element ];
       let type = typeof element_value;
-      if (type === 'string' || type === 'number' || type === 'boolean') {
+      if (type === 'string' || type === 'number' || type === 'boolean' ) {
         add_story_row( var_name, element , element_value, story );
         if ( type === 'boolean' ) {
           were_checkboxes = true;
@@ -171,7 +172,7 @@ let parse_elements = function(var_name, elements, story) {
           story: story,
         });
       } else {
-        console.log('Sorry, something has not been accounted for. You will have to do some editing to get this to work. (1)', var_name, typeof choice_value);
+        console.warn( 'Sorry, something has not been accounted for. You will have to do some editing to get this to work. (1)', var_name, typeof choice_value);
       }
     }  // ends for choice in elements
     // In case all elements were false, we
@@ -191,20 +192,19 @@ let parse_dict = function({name_start, choice, dict, story}, debug) {
     let instanceName = dict.instanceName;
     if ( instanceName ) {
       add_story_row( name_start, '', instanceName, story );
-      console.log( `${name_start}, , ${instanceName}`);
     }
 
     let var_name = key;
-    if (name_start !== '') {
+    if (name_start !== '' ) {
       var_name = name_start + '.' + key;
     }
     let value = dict[ key ];
 
     let val_type = typeof value;
-    if (val_type === 'string' || val_type === 'number') {
+    if (val_type === 'string' || val_type === 'number' ) {
       add_story_row( var_name, '', value, story );
 
-    } else if (val_type === 'boolean') {
+    } else if (val_type === 'boolean' ) {
       add_bool(var_name, value, story);
 
     } else if (Array.isArray(value)) {
@@ -239,74 +239,11 @@ let get_story = function( vars, story ) {
 
 
 // ============================
-// Extra options
-// ============================
-
-// let ignore_keys_input = document.getElementById('ignore_keys');
-// ignore_keys_input.value = JSON.stringify( keys_to_ignore, null, 2 );
-
-
-// ============================
-// Triggering and getting the result
-// ============================
-
-let scenario = document.getElementById('scenario');
-let tableInput = document.getElementById('data');
-let output = '';
-tableInput.addEventListener('input', function() {
-  
-  let input = tableInput.value || '{}';
-  let data = null;
-  let vars = null;
-  try {
-    data = JSON.parse( input );
-    vars = data.variables;
-  } catch (err) {
-    data = toString(err);
-  }
-  
-  if ( typeof data === 'string' ) {
-    console.log( data );
-    output = `\n\n${ test_start }`;
-    output += '\nSee console for output error';
-  } else {
-    let story = [];
-    get_story( vars, story );
-
-    let test_length = `slow`;
-    if ( story.length <= 20 ) { test_length = `fast`; }
-    output = `\n\n@generated @${ test_length }`;
-    output += test_start;
-
-    for ( let row of story ) {
-      output += `\n\u00A0\u00A0\u00A0\u00A0${row}`;
-    }
-
-  }  // ends if error
-
-  scenario.innerText = output;
-});  // ends text area event listener
-
-
-// ============================
-// Copying
-// ============================
-let copy_scenario_button = document.getElementById('copy_scenario');
-copy_scenario_button.addEventListener('click', ( elem ) => {
-  navigator.clipboard.writeText( output );
-});
-
-
-let copy_feature_button = document.getElementById('copy_feature');
-let feature = document.getElementById('feature');
-copy_feature_button.addEventListener('click', ( elem ) => {
-  navigator.clipboard.writeText( `${ feature.innerText }\n\n` );
-});
-
-
-// ============================
 // Prepping start (and default) text
 // ============================
+let scenario = document.getElementById( 'scenario' );
+let output = '';
+
 let test_start = `\nScenario: Quick description of specific example`;
 test_start += `\n\u00A0\u00A0Given I start the interview at "interview_YAML_file_name"`
 test_start += `\n\u00A0\u00A0And the user gets to "some question id" with this data:`
@@ -315,3 +252,140 @@ test_start += `\n\u00A0\u00A0\u00A0\u00A0| var | choice | value |`;
 scenario.innerText = test_start;
 output = `\n\n${ test_start }`;
 
+
+
+// ============================
+// Getting and showing the result
+// ============================
+let da_warning = document.getElementById( 'da_data_warning' );
+let tableInput = document.getElementById( 'da_data' );
+let data_error = document.querySelector( 'section#data_container .error_output' );
+
+let update_output = function () {
+  let data = null;
+  let vars = null;
+  try {
+    data = JSON.parse( tableInput.value );
+    vars = data.variables;
+    da_warning.classList.remove('error');
+    data_error.innerText = '';
+  } catch (err) {
+    if ( tableInput.value !== '' ) {
+      console.warn( data );
+      da_warning.classList.add('error');
+      data_error.innerText = err;
+      output = `\n\n${ test_start }`;
+    }
+    return;
+  }
+  
+  let story = [];
+  get_story( vars, story );
+
+  let test_length = `slow`;
+  if ( story.length <= 20 ) { test_length = `fast`; }
+  output = `\n\n@generated @${ test_length }`;
+  output += test_start;
+
+  for ( let row of story ) {
+    output += `\n\u00A0\u00A0\u00A0\u00A0${row}`;
+  }
+
+  scenario.innerText = output;
+};
+
+tableInput.addEventListener( 'input', update_output );  // ends text area event listener
+
+
+
+// ============================
+// Extra options
+// ============================
+let auto_ignored = document.getElementById( 'auto_ignored_keys_only' );
+auto_ignored.innerText = JSON.stringify( keys_to_ignore );
+
+let ignore_anywhere = [];
+let ignore_warning = document.getElementById( 'ignore_warning' );
+let ignore_error = document.querySelector( 'section#ignore_lists_container .error_output' );
+
+let to_fit = ['ignore_anywhere'];
+document.body.addEventListener( 'input', function( event ) {
+  if ( to_fit.includes( event.target.id )) {
+    let area = event.target;
+    let num_new_lines = (area.value.split( '\n' )).length;
+    area.rows = num_new_lines;
+  }
+
+  if ( event.target.id === 'ignore_anywhere' ) {
+    if ( ignore_node.value ) {
+      try {
+        ignore_anywhere = JSON.parse( ignore_node.value );
+        ignore_warning.classList.remove('error');
+        ignore_error.innerText = '';
+        update_output
+      } catch (err) {
+        console.warn(err);
+        ignore_warning.classList.add('error');
+        ignore_error.innerText = err;
+        ignore_anywhere = ignore_anywhere_default_alphabetical;  // use default
+      }
+    } else if ( ignore_node.value === '' ) {
+      ignore_anywhere = [];
+    }
+    update_output();
+  }
+});  // ends listen for input
+
+let ignore_node = document.getElementById( 'ignore_anywhere' );
+let ignore_anywhere_default_alphabetical = ignore_anywhere_default.sort(function (a, b) {
+    if (a > b) { return 1; }
+    if (b > a) { return -1; }
+    return 0;
+});
+let reset_ignore_elem = function () {
+  // Ignore text we should ignore wherever it appears, even in a fully formed variable name
+  let ignore_node_initial_text = JSON.stringify( ignore_anywhere_default_alphabetical, null, 2 );
+  let num_new_lines = (ignore_node_initial_text.split( '\n' )).length;
+  ignore_node.rows = num_new_lines;
+  ignore_node.value = ignore_node_initial_text;
+  ignore_anywhere = ignore_anywhere_default_alphabetical;
+  update_output();
+};
+reset_ignore_elem();
+
+// 'click' listeners
+document.body.addEventListener( 'click', ( event ) => {
+  if ( /toggler/.test(event.target.className)) {
+    let toggler = event.target
+    let id = toggler.getAttribute( 'for' );
+
+    let to_toggle = document.getElementById( id );
+    if ( !/expanded/.test( to_toggle.className )) {
+      to_toggle.classList.add( 'expanded' );
+      toggler.innerText = toggler.innerText.replace( 'Edit', 'Hide' );
+      toggler.innerText = toggler.innerText.replace( '▼', '▲' );
+    } else {
+      to_toggle.classList.remove( 'expanded' );
+      toggler.innerText = toggler.innerText.replace( 'Hide', 'Edit' );
+      toggler.innerText = toggler.innerText.replace( '▲', '▼' );
+    }
+  } if ( event.target.id === 'reset_ignore' ) {
+    reset_ignore_elem();
+  }
+});  // End listen for click
+
+
+// ============================
+// Copying test stuff
+// ============================
+let copy_scenario_button = document.getElementById( 'copy_scenario' );
+copy_scenario_button.addEventListener( 'click', ( elem ) => {
+  navigator.clipboard.writeText( output );
+});
+
+
+let copy_feature_button = document.getElementById( 'copy_feature' );
+let feature = document.getElementById( 'feature' );
+copy_feature_button.addEventListener( 'click', ( elem ) => {
+  navigator.clipboard.writeText( `${ feature.innerText }\n\n` );
+});
