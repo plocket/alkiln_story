1f26927bf28d4a350ff800ef6addc2366835139b
plocket <52798256+plocket@users.noreply.github.com>
2021-05-31 09:35:28 -0400

    Fix #12, strings broken into arrays

--- generate.js
+++ generate.js
@@ -15,13 +15,27 @@ let keys_to_ignore = [
 // Ignore text we should ignore wherever it appears, even in a fully formed variable name
 let ignore_anywhere_default = [
   // === AssemblyLine-specific keywords to skip
-  'interview_metadata',
-  'macourts',
+  'all_courts',
+  '_attachment[i]',
   'court_emails',
+  'download_titles',
+  'form_approved_for_email_filing',
+  'github_user',
+  'interview_metadata',
+  'interview_short_title',
+  'macourts',
+  'package_name',
+  'package_version_number',
+  'preferred_court',
+  'signature_fields',
+  'speak_text',
+  'started_on_phone',
+  'user_has_saved_answers',
   '.location',
   'github_repo_name',
   'allow_cron',
   'allowed_courts',
+  '_geocoded',
   '.uses_parts',
   `.court_code`,
   `.department`,
@@ -131,33 +145,30 @@ let get_story_row = function({ name, value, checked }, debug=false) {
 let parse = {};
 
 parse.all = function ({name, value, checked }, debug) {
+  // Filters - makes sure each item in processed by the
+  // right function based on its type and constructs the story.
+  // Also puts together the whole story. Maybe break this up into two
+  // parts.
   let all = [];
 
-  for ( let key in value ) {
-    if ( keys_to_ignore.includes( key )) { continue; }
-    if ( ignore_anywhere_default.includes( key )) { continue; }  // logging everything was annoying
+  if ( keys_to_ignore.includes( name )) { return all; }
+  if ( ignore_anywhere_default.includes( name )) { return all; }  // logging everything was annoying
 
-    let var_name = key;
-    if (name !== '' ) { var_name = name + '.' + key; }
-    let new_value = value[ key ];
-    let val_type = typeof new_value;
+  let var_name = name;
+  let val_type = typeof value;
 
-    let rows = [];
-    if ( Array.isArray( new_value )) {
-      rows = parse.array({ name: var_name, value: new_value, checked: '', }, debug);
+  if ( Array.isArray( value )) {
+    all = parse.array({ name: var_name, value: value, checked: '', }, debug);
 
-    } else if ( new_value === null ) {
-      rows = parse.null({ name: var_name, value: new_value, checked: '', }, debug);
+  } else if ( value === null ) {
+    all = parse.null({ name: var_name, value: value, checked: '', }, debug);
 
-    } else if ( new_value !== null && parse[ val_type ] ) {
-      rows = parse[ val_type ]({ name: var_name, value: new_value, checked: '', }, debug);
-    
-    } else {
-      console.warn( 'Sorry, something has not been accounted for. You will have to do some editing to get this to work.', var_name, typeof value);
-    }
-
-    all = all.concat( rows );
-  }  // ends for key in value
+  } else if ( value !== null && parse[ val_type ] ) {
+    all = parse[ val_type ]({ name: var_name, value: value, checked: '', }, debug);
+  
+  } else {
+    console.warn( 'Sorry, something has not been accounted for. You will have to do some editing to get this to work.', var_name, typeof value);
+  }
 
   // Get unique rows that are strings (removing `undefined`)
   let story = [];
@@ -220,11 +231,32 @@ parse.elements = function ({ name, value, checked }, debug) {
 
 parse.object = function ({ name, value, checked }, debug) {
   if ( value.instanceName ) { name = value.instanceName; }
+
+  // `elements` can be (are always?) checkbox items
   let { elements, ...new_obj } = value;
-  if ( elements ) {  // Skip adding `element` to the name
+  if ( elements ) {
+    // Skip adding `element` to the name and don't get any other props
+    // This `return`` might be what skips objects like trial_court
     return parse.elements({ name, value: elements, checked: '', }, debug);
   }
-  return parse.all({ name, value: new_obj, checked: '', }, debug);
+
+  let the_rest = [];
+  for ( let key in new_obj ) {
+    if ( keys_to_ignore.includes( key )) { continue; }
+    if ( ignore_anywhere_default.includes( key )) { continue; }  // logging everything was annoying
+
+    let var_name = key;
+    if (name !== '' ) { var_name = name + '.' + key; }
+    let new_value = new_obj[ key ];
+    let val_type = typeof new_value;
+    // Send the contents of the object to be filtered
+    let rows = parse.all({ name: var_name, value: new_value, checked: '', }, debug);
+
+    the_rest = the_rest.concat( rows );
+  }  // ends for key in new_obj
+
+  return the_rest;
+  // return parse.all({ name, value: new_obj, checked: '', }, debug);
 };  // Ends parse.object()
 
 
@@ -362,7 +394,7 @@ let update_output = function () {
 
     // Add early part of test
     let test_length = `slow`;
-    if ( story.length <= 20 ) { test_length = `fast`; }
+    if ( story.length <= 45 ) { test_length = `fast`; }
     output += ` @${ test_length }`;
 
   // If no input or erroring input
